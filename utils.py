5b69ea722c5b7f7332c33cb3fcc9f4dd5cad71c1
Ilya Nasiri <ina14@sfu.ca>
2024-03-31 01:40:45 -0700

    Added a function to make a training/testing split on a CSV dataset, and turn it into a torch tensor (by default) on the appropriate device (GPU/CPU)

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 import pandas as pd
+import torch
 
 
 def data_to_moving_window(X: pd.DataFrame, attribute: str, window_size: int):
@@ -34,3 +35,33 @@ def csv_to_moving_window(path: str, attribute: str, window_size: int):
     data =  pd.read_csv(path)[ attribute ].to_numpy().squeeze()
     data = sliding_window_view(data, window_size)
     return data.astype(np.float32)
+
+def get_windowed_data_splits(path, attr, window_size, percentage, convert_to_tensor=True, allow_gpu=True):
+    """
+    :param path: path to the preprocessed CSV data file
+    :param attr: attrbute to retrieve from the CSV
+    :param window_size: sliding window size
+    :param percentage: how large is the training dataset
+    :param convert_to_tensor whether to convert the data to torch tensor
+    :param allow_gpu: whether to allow using GPU tensor if CUDA is available
+    :return: returns tensors on the device (cpu/gpu):
+    windowed_data:
+    X_train:
+    y_train:
+    X_test:
+    y_test
+    """
+    if allow_gpu:
+        device = "cuda" if (torch.cuda.is_available() and allow_gpu) else "cpu"
+
+    windowed_data = csv_to_moving_window(path, attr, window_size)
+    X = windowed_data[:, :-1]
+    y = windowed_data[:, -1]
+
+    if convert_to_tensor:
+        X = torch.tensor(X).view(X.shape[0], X.shape[1], 1).to(device)
+        y = torch.tensor(y).view(y.shape[0], 1).to(device)
+
+    split_point = int(X.shape[0] * percentage)
+    X_train, y_train, X_test, y_test = X[:split_point], y[:split_point], X[split_point:], y[split_point:]
+    return windowed_data, X_train, y_train, X_test, y_test
