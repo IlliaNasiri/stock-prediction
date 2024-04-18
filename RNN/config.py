926a89d52735e70e892895b393f466043f6a90cb
Ilya Nasiri <ina14@sfu.ca>
2024-04-17 23:30:07 -0700

    Made the config more neat by putting some parameters as extra variables.

--- RNN/config.py
+++ RNN/config.py
@@ -1,15 +1,19 @@
+N_HIDDEN = 64
+LABEL_WIDTH = 5
+WINDOW_SIZE = 15
+
 ModelConfig = {
     "n_features": 1,
-    "n_hidden": 64,
+    "n_hidden": N_HIDDEN,
     "num_rnn_layers": 2,
-    "dense_layers": [(64, 256), (256, 512), (512, 5)]
+    "dense_layers": [(N_HIDDEN, 256), (256, 512), (512, LABEL_WIDTH)]
 }
 
 TrainingConfig = {
     "data_path": "data/IBM.csv",
     "attribute": "Close",
-    "window_size": 15,
-    "label_width": 5,
+    "window_size": WINDOW_SIZE,
+    "label_width": LABEL_WIDTH,
     "train_size": 1.0,
     "epochs": 300,
     "model_save_path": "RNN/trained_models/rnn.pth"
@@ -18,8 +22,8 @@ TrainingConfig = {
 TestingConfig = {
     "data_path": "data/MSFT.csv",
     "attribute": "Close",
-    "window_size": 15,
-    "label_width": 5,
+    "window_size": WINDOW_SIZE,
+    "label_width": LABEL_WIDTH,
     "test_size": 0.1,
     "model_save_path": "RNN/trained_models/rnn.pth",
     "visualization_save_path": "RNN/visualizations/comparison.jpg"
